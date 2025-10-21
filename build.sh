7a2f785f8be4563b61e709ecbdcfa93e34d29023
nthe99124 <nthe99124@gmail.com>
2025-10-21 12:33:14 +0700

    sửa docker

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ docker build -t autoappmanagement-webapp:lastest -f webapp.Dockerfile .
 
 COMPOSE_FILE="docker-compose-build.yml"
 
-docker-compose -f %COMPOSE_FILE% down --remove-orphans
+docker compose -f %COMPOSE_FILE% down --remove-orphans
 
-docker-compose -f $COMPOSE_FILE up -d
+docker compose -f $COMPOSE_FILE up -d
